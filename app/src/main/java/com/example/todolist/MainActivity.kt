b0d4b0534a38191df60bbf391343685051d4a685
Your Name <you@example.com>
2023-03-08 07:47:00 +0530

    Todo application using android development

---
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -0,0 +1,73 @@
+package com.example.todolist
+
+import android.app.AlertDialog
+import android.content.DialogInterface
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ListView
+
+lateinit var item : EditText
+lateinit var add : Button
+lateinit var listView : ListView
+
+var itemList = ArrayList<String>()
+
+var fileHelper = FileHelper()
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        item = findViewById(R.id.editText)
+        add = findViewById(R.id.button)
+        listView = findViewById(R.id.list)
+
+        itemList = fileHelper.readData(this)
+
+        var arrayAdapter = ArrayAdapter(this,android.R.layout.simple_list_item_1
+            ,android.R.id.text1,itemList)
+        listView.adapter = arrayAdapter
+
+        add.setOnClickListener {
+
+            var itemName : String = item.text.toString()
+            itemList.add(itemName)
+            item.setText("")
+            fileHelper.writeData(itemList,applicationContext)
+            arrayAdapter.notifyDataSetChanged()
+
+        }
+
+        listView.setOnItemClickListener { adapterView, view, position, l ->
+
+            var alert = AlertDialog.Builder(this)
+            alert.setTitle("Delete")
+            alert.setMessage("Do you want to delete this item from the list?")
+            alert.setCancelable(false)
+            alert.setNegativeButton("No", DialogInterface.OnClickListener { dialogInterface, i ->
+
+                dialogInterface.cancel()
+
+            })
+            alert.setPositiveButton("Yes", DialogInterface.OnClickListener { dialogInterface, i ->
+
+                itemList.removeAt(position)
+                arrayAdapter.notifyDataSetChanged()
+                fileHelper.writeData(itemList,applicationContext)
+
+            })
+
+            alert.create().show()
+
+        }
+
+
+
+    }
+}
